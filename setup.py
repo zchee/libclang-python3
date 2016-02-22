ba51c9e2eac0a3df92b711df6ce76b03f615a055
Koichi Shiraishi <k@zchee.io>
2016-02-23 07:15:12 +0900

    WIP: Rename package name and other informations
    - Not exists original auther name and email
      - libclang-py3 original author: Matthaeus G. Chajdas
      - libclang-py3 original email: dev@anteru.net
    - What is better way?
    
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 
 from setuptools import setup
 
-setup(name="libclang-py3",
-      version="0.3",
-      description="Python3 bindings for libclang ",
-      url="https://bitbucket.org/Anteru/python3-libclang",
-      download_url="https://bitbucket.org/Anteru/python3-libclang/releases",
+setup(name="clang-python3",
+      version="0.0.1",
+      description="Python3 bindings for libclang",
+      url="https://github.com/zchee/clang-python3",
+      download_url="https://github.com/zchee/clang-python3/releases",
       license="License :: OSI Approved :: University of Illinois/NCSA Open Source License",
       classifiers=[
           "Intended Audience :: Developers",
@@ -17,8 +17,8 @@ setup(name="libclang-py3",
           "Topic :: Software Development :: Compilers"
       ],
       keywords=["llvm", "clang", "libclang"],
-      author="Matthaeus G. Chajdas",
-      author_email="dev@anteru.net",
+      author="Koichi Shiraishi aka zchee",
+      author_email="k@zchee.io",
       zip_safe=False,
       packages=["clang"]
 )
