f91bacc24d28980c7a1f1a1144e9b3f7ad8ae7c3
Koichi Shiraishi <zchee.io@gmail.com>
2018-08-27 12:45:21 +0900

    Upstream: [libclang] Expose the ElaboratedType
    Differential Revision: http://reviews.llvm.org/D11797
    
    git-svn-id: llvm.org/svn/llvm-project/cfe/trunk@268366 91177308-0d34-0410-b5e6-96231b3b80d8
    
    author: skalinichev
    llvm-mirror: llvm-mirror/clang@7cd277a

--- clang/cindex.py
+++ clang/cindex.py
@@ -1877,6 +1877,7 @@ TypeKind.VARIABLEARRAY = TypeKind(115)
 TypeKind.DEPENDENTSIZEDARRAY = TypeKind(116)
 TypeKind.MEMBERPOINTER = TypeKind(117)
 TypeKind.AUTO = TypeKind(118)
+TypeKind.ELABORATED = TypeKind(119)
 
 class RefQualifierKind(BaseEnumeration):
     """Describes a specific ref-qualifier of a type."""
@@ -2101,6 +2102,12 @@ class Type(Structure):
         Retrieve the class type of the member pointer type.
         """
         return conf.lib.clang_Type_getClassType(self)
+
+    def get_named_type(self):
+        """
+        Retrieve the type named by the qualified-id.
+        """
+        return conf.lib.clang_Type_getNamedType(self)
 
     def get_align(self):
         """
@@ -3720,6 +3727,11 @@ functionList = [
   ("clang_Type_getCXXRefQualifier",
    [Type],
    c_uint),
+
+  ("clang_Type_getNamedType",
+   [Type],
+   Type,
+   Type.from_result),
 
   ("clang_Type_visitFields",
    [Type, callbacks['fields_visit'], py_object],
